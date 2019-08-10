a75bb1253b940abbda3d156781631eb93301bb7b
flaviogf <flavio.fernandes6@gmail.com>
2019-08-10 09:29:34 -0300

    restart

--- manage.py
+++
@@ -1,3 +0,0 @@
-from match_games import create_app
-
-app = create_app(config='config.Production')
--- match_games/stores/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint('stores', __name__)
-
-
-@blueprint.route('/stores')
-def stores():
-    return 'OK'
