e371eac1aefaf2647946ceb7bd40be43c9aea60d
flaviogf <flavio.fernandes6@gmail.com>
2019-08-21 22:38:31 -0300

    Add list store and list games

--- frontend/src/pages/AdminGameStore/index.jsx
+++ frontend/src/pages/AdminGameStore/index.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import AdminTemplate from '../../components/AdminTemplate';
 
 import { Content, Form, Title, Select, Button } from './styles';
 
+import api from '../../services/api';
+
 export default function AdminGameStore() {
+  const [games, setGames] = useState([]);
+  const [stores, setStores] = useState([]);
+
+  useEffect(() => {
+    function loadGames() {
+      api
+        .get('/api/v1/games')
+        .then((res) => res.data.data)
+        .then(setGames)
+        .catch(console.error);
+    }
+
+    function loadStores() {
+      api
+        .get('/api/v1/stores')
+        .then((res) => res.data.data)
+        .then(setStores)
+        .catch(console.error);
+    }
+
+    loadGames();
+    loadStores();
+  }, []);
+
   function onSubmit(e) {
     e.preventDefault();
   }
@@ -16,12 +42,18 @@ export default function AdminGameStore() {
           <Title>Game &gt; Store</Title>
 
           <Select>
-            <option>Crash</option>
-            <option>Fifa 19</option>
+            {games.map((game) => (
+              <option key={game.id} value={game.id}>
+                {game.name}
+              </option>
+            ))}
           </Select>
           <Select>
-            <option>Shop B</option>
-            <option>Big Boy</option>
+            {stores.map((store) => (
+              <option key={store.id} value={store.id}>
+                {store.name}
+              </option>
+            ))}
           </Select>
 
           <Button>Salvar</Button>
