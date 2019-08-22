476fd162a4af79802c3b8ae1c8fb6490a8f66660
flaviogf <flavio.fernandes6@gmail.com>
2019-08-22 11:21:33 -0300

    Change environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { toast } from 'react-toastify';
 
-const env = 'development';
+const env = 'production';
 
 const production = 'https://backend.matchgames.flaviogf.com.br';
 
