5eeee06fc4c6fd0dfb7780fbe940d3c109cdf6f6
flaviogf <flavio.fernandes6@gmail.com>
2019-08-21 22:52:44 -0300

    Fix error_messages serializer game store

--- backend/match_games/game_store/serializers.py
+++ backend/match_games/game_store/serializers.py
@@ -5,11 +5,11 @@ from match_games import ma
 
 class CreateGameStoreSerializer(ma.Schema):
     game_id = fields.Integer(required=True,
-                             errors_message={'required': 'Game must be informed.'})
+                             error_messages={'required': 'Game must be informed.'})
     store_id = fields.Integer(required=True,
-                              errors_message={'required': 'Store must be informed.'})
+                              error_messages={'required': 'Store must be informed.'})
     value = fields.Float(required=True,
-                         errors_message={'required': 'Value must be informed.'})
+                         error_messages={'required': 'Value must be informed.'})
 
 
 create_game_store_serializer = CreateGameStoreSerializer(unknown=EXCLUDE)
